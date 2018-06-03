9af90c8bf00af09d2d86de627a257d03f0d91fae
Robert Yuen <dungeonry@hotmail.com>
2018-06-03 14:11:27 -0700

    Changes were made.

--- server.js
+++ server.js
@@ -2,11 +2,12 @@ const express = require('express')
 const path = require('path')
 const serveStatic = require('serve-static');
 const helmet = require("helmet");
+const PORT = process.env.PORT || 3000;
 
 const app = express()
 
 app.use(helmet());
 app.use(serveStatic(path.join(__dirname, 'public/images')))
-app.listen(3000, () => {
-    console.log("PORT 3000");
+app.listen(PORT, () => {
+    console.log(`NODE STATIC SERVER is on PORT ${PORT}.`);
 });
